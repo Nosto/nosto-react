825b479cd9ee697d29a3e576a449bcbebb1d6b83
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-05-25 17:22:45 +0200

    removed react router

--- src/components/Placement/index.tsx
+++ src/components/Placement/index.tsx
@@ -1,31 +1,11 @@
-import React, { MouseEvent } from "react";
-
-import { useNavigate } from "react-router-dom";
+import React from "react";
 
 export interface PlacementProps {
   id: string;
 }
 
 const Placement: React.FC<PlacementProps> = ({ id }) => {
-  let navigate = useNavigate();
-  console.log(useNavigate);
-  console.log(navigate);
-
-  const handleClick = (e: MouseEvent) => {
-    // noinspection TypeScriptUnresolvedFunction
-    // @ts-ignore
-    const targetLink = e.target.closest("a");
-    if (!targetLink) {
-      return;
-    } else {
-      e.preventDefault();
-      navigate(
-        targetLink.href.toString().replace(new URL(targetLink.href).origin, "")
-      );
-    }
-  };
-
-  return <div className="nosto_element" id={id} onClick={handleClick} />;
+  return <div className="nosto_element" id={id} />;
 };
 
 export default Placement;

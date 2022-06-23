1cc260750db311b9a221c5cbfaf4d39670b16200
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-23 18:49:50 +0200

    added condition for API call based on clientScriptLoaded

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,20 +1,26 @@
 import React, { useEffect } from "react";
+import { useNostoContext } from "../Provider/context.client";
 
 const NostoHome: React.FC = () => {
+  console.log("HOME RENDERED 2");
+  const { clientScriptLoaded } = useNostoContext();
+  console.log(clientScriptLoaded);
   useEffect(() => {
     // @ts-ignore
-    window.nostojs((api) => {
-      api
-        .defaultSession()
-        .setResponseMode("HTML")
-        .viewFrontPage()
-        .setPlacements(api.placements.getPlacements())
-        .load()
-        .then((data: object) => {
-          // @ts-ignore
-          api.placements.injectCampaigns(data.recommendations);
-        });
-    });
+    if (clientScriptLoaded) {
+      window.nostojs((api: any) => {
+        api
+          .defaultSession()
+          .setResponseMode("HTML")
+          .viewFrontPage()
+          .setPlacements(api.placements.getPlacements())
+          .load()
+          .then((data: object) => {
+            // @ts-ignore
+            api.placements.injectCampaigns(data.recommendations);
+          });
+      });
+    }
   }, []);
 
   return (

9f6e0d366d81441a3e9fb3f0eca725702c56f6cf
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-23 18:23:38 +0200

    export useNostoContext, add .client for context file name

---
+++ src/components/Provider/context.client.ts
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export interface NostoInterface {
+  account: string;
+}
+
+/* tslint:disable:no-empty */
+export const NostoContext = createContext<NostoInterface>({
+  // @ts-ignore
+  account: undefined
+});
+
+/* tslint:enable:no-empty */
+export function useNostoContext(){
+  const context = useContext(NostoContext);
+
+  if (!context) {
+    throw new Error('No nosto context found');
+  }  
+
+  return context;
+}

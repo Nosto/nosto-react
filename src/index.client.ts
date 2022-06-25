3281c3cf65f38b0c5e7bfecd2b9b91364c33e601
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-06-25 09:36:27 +0200

    added .client for all Nosto component files

--- src/index.client.ts
+++ src/index.client.ts
@@ -8,26 +8,30 @@ declare global {
 
 export * from "./types";
 // noinspection JSUnusedGlobalSymbols
-export { default as Nosto404 } from "./components/Fohofo";
+export { default as Nosto404 } from "./components/Fohofo/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoOther } from "./components/Other";
+export { default as NostoOther } from "./components/Other/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoCheckout } from "./components/Checkout";
+export { default as NostoCheckout } from "./components/Checkout/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoProduct } from "./components/Product";
+export { default as NostoProduct } from "./components/Product/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoCategory } from "./components/Category";
+export { default as NostoCategory } from "./components/Category/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoSearch } from "./components/Search";
+export { default as NostoSearch } from "./components/Search/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoOrder } from "./components/Order";
+export { default as NostoOrder } from "./components/Order/index.client";
 // noinspection JSUnusedGlobalSymbols
 export { default as NostoHome } from "./components/Home/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoPlacement } from "./components/Placement";
+export { default as NostoPlacement } from "./components/Placement/index.client";
 // noinspection JSUnusedGlobalSymbols
 export { default as NostoProvider } from "./components/Provider/index.client";
 // noinspection JSUnusedGlobalSymbols
-export { NostoContext, useNostoContext } from "./components/Provider/context.client";
+export {
+  NostoContext,
+  useNostoContext,
+} from "./components/Provider/context.client";
 // noinspection JSUnusedGlobalSymbols
-export { default as NostoSession } from "./components/Session";
+export { default as NostoSession } from "./components/Session/index.client";
+//

c00a16c83f679044e325c60415e8c9c57c2b3fb4
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2022-06-25 09:46:37 +0200

    added clientScriptLoaded condition to Category component

--- src/components/Category/index.client.tsx
+++ src/components/Category/index.client.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from "react";
+import { useNostoContext } from "../Provider/context.client";
 
 const NostoCategory: React.FC<{ category: string }> = ({ category }) => {
+  const { clientScriptLoaded } = useNostoContext();
   useEffect(() => {
     // @ts-ignore
-    window.nostojs((api) => {
-      api
-        .defaultSession()
-        .setResponseMode("HTML")
-        .viewCategory(category)
-        .setPlacements(api.placements.getPlacements())
-        .load()
-        .then((data: object) => {
-          // @ts-ignore
-          api.placements.injectCampaigns(data.recommendations);
-        });
-    });
-  }, []);
+    if (clientScriptLoaded) {
+      window.nostojs((api) => {
+        api
+          .defaultSession()
+          .setResponseMode("HTML")
+          .viewCategory(category)
+          .setPlacements(api.placements.getPlacements())
+          .load()
+          .then((data: object) => {
+            // @ts-ignore
+            api.placements.injectCampaigns(data.recommendations);
+          });
+      });
+    }
+  }, [clientScriptLoaded, category]);
 
   return (
     <>

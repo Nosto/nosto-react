2804436081b7fbed7826bd598ff7e907cbac44ae
dominikgilg <74245822+dominikgilg@users.noreply.github.com>
2022-06-28 16:22:07 +0200

    CT-19 Updates for PDP (#25)
    * added clientScriptLoaded and currentVariation support within NostoProduct component
    * Edited syntax for conditional tagging rendering

--- src/components/Product/index.client.tsx
+++ src/components/Product/index.client.tsx
@@ -1,27 +1,30 @@
 import { Product } from "../../types";
-import stringinate from "../../utils/stringinate";
 import React, { useEffect } from "react";
-import snakeize from "snakeize";
+import { useNostoContext } from "../Provider/context.client";
 
 const NostoProduct: React.FC<{ product: string; tagging: Product }> = ({
   product,
   tagging,
 }) => {
+  const { clientScriptLoaded, currentVariation } = useNostoContext();
   useEffect(() => {
     // @ts-ignore
-    window.nostojs((api) => {
-      api
-        .defaultSession()
-        .setResponseMode("HTML")
-        .viewProduct(snakeize(stringinate(tagging)))
-        .setPlacements(api.placements.getPlacements())
-        .load()
-        .then((data: object) => {
-          // @ts-ignore
-          api.placements.injectCampaigns(data.recommendations);
-        });
-    });
-  }, []);
+    if (clientScriptLoaded) {
+      window.nostojs((api: any) => {
+        api
+          .defaultSession()
+          .setVariation(currentVariation)
+          .setResponseMode("HTML")
+          .viewProduct(product)
+          .setPlacements(api.placements.getPlacements())
+          .load()
+          .then((data: object) => {
+            // @ts-ignore
+            api.placements.injectCampaigns(data.recommendations);
+          });
+      });
+    }
+  }, [clientScriptLoaded, currentVariation]);
 
   return (
     <>
@@ -29,81 +32,79 @@ const NostoProduct: React.FC<{ product: string; tagging: Product }> = ({
         product
       </div>
       <div className="nosto_product" style={{ display: "none" }}>
-        {tagging.variationId && (
+        {tagging?.variationId && (
           <span className="variation_id">{tagging.variationId}</span>
         )}
-        {tagging.productId && (
-          <span className="product_id">{tagging.productId}</span>
-        )}
-        {tagging.name && <span className="name">{tagging.name}</span>}
-        {tagging.url && <span className="url">{tagging.url.toString()}</span>}
-        {tagging.imageUrl && (
+        {product && <span className="product_id">{product}</span>}
+        {tagging?.name && <span className="name">{tagging.name}</span>}
+        {tagging?.url && <span className="url">{tagging.url.toString()}</span>}
+        {tagging?.imageUrl && (
           <span className="image_url">{tagging.imageUrl.toString()}</span>
         )}
-        {tagging.availability && (
+        {tagging?.availability && (
           <span className="availability">{tagging.availability}</span>
         )}
-        {tagging.price && <span className="price">{tagging.price}</span>}
-        {tagging.listPrice && (
+        {tagging?.price && <span className="price">{tagging.price}</span>}
+        {tagging?.listPrice && (
           <span className="list_price">{tagging.listPrice}</span>
         )}
-        {tagging.priceCurrencyCode && (
+        {tagging?.priceCurrencyCode && (
           <span className="price_currency_code">
             {tagging.priceCurrencyCode}
           </span>
         )}
-        {tagging.brand && <span className="brand">{tagging.brand}</span>}
-        {tagging.description && (
+        {tagging?.brand && <span className="brand">{tagging.brand}</span>}
+        {tagging?.description && (
           <span className="description">{tagging.description}</span>
         )}
-        {tagging.googleCategory && (
+        {tagging?.googleCategory && (
           <span className="description">{tagging.googleCategory}</span>
         )}
-        {tagging.condition && (
+        {tagging?.condition && (
           <span className="condition">{tagging.condition}</span>
         )}
-        {tagging.gender && <span className="gender">{tagging.gender}</span>}
-        {tagging.ageGroup && (
+        {tagging?.gender && <span className="gender">{tagging.gender}</span>}
+        {tagging?.ageGroup && (
           <span className="age_group">{tagging.ageGroup}</span>
         )}
-        {tagging.gtin && <span className="gtin">{tagging.gtin}</span>}
-        {tagging.category &&
-          tagging.category.map((category, index) => (
+        {tagging?.gtin && <span className="gtin">{tagging.gtin}</span>}
+        {tagging?.category &&
+          tagging?.category.map((category, index) => (
             <span className="category" key={index}>
               {category}
             </span>
           ))}
-        {tagging.tags1 &&
+        {tagging?.tags1 &&
           tagging.tags1.map((tag, index) => (
             <span className="tag1" key={index}>
               {tag}
             </span>
           ))}
-        {tagging.tags2 &&
+        {tagging?.tags2 &&
           tagging.tags2.map((tag, index) => (
             <span className="tag2" key={index}>
               {tag}
             </span>
           ))}
-        {tagging.tags3 &&
+        {tagging?.tags3 &&
           tagging.tags3.map((tag, index) => (
             <span className="tag3" key={index}>
               {tag}
             </span>
           ))}
-        {tagging.ratingValue && (
+        {tagging?.ratingValue && (
           <span className="rating_value">{tagging.ratingValue}</span>
         )}
-        {tagging.reviewCount && (
+        {tagging?.reviewCount && (
           <span className="review_count">{tagging.reviewCount}</span>
         )}
-        {tagging.alternateImageUrls &&
+        {tagging?.alternateImageUrls &&
           tagging.alternateImageUrls.map((url, index) => (
             <span className="alternate_image_url" key={index}>
               {url.toString()}
             </span>
           ))}
-        {tagging.customFields &&
+        {tagging?.customFields &&
           Object.keys(tagging.customFields).map(
             (field, index) =>
               tagging.customFields &&
@@ -113,24 +114,24 @@ const NostoProduct: React.FC<{ product: string; tagging: Product }> = ({
                 </span>
               )
           )}
-        {tagging.skus &&
+        {tagging?.skus &&
           tagging.skus.map((sku, index) => (
             <span className="nosto_sku" key={index}>
-              {sku.id && <span className="product_id">{sku.id}</span>}
-              {sku.name && <span className="name">{sku.name}</span>}
-              {sku.price && <span className="price">{sku.price}</span>}
-              {sku.listPrice && (
+              {sku?.id && <span className="product_id">{sku.id}</span>}
+              {sku?.name && <span className="name">{sku.name}</span>}
+              {sku?.price && <span className="price">{sku.price}</span>}
+              {sku?.listPrice && (
                 <span className="list_price">{sku.listPrice}</span>
               )}
-              {sku.url && <span className="url">{sku.url.toString()}</span>}
-              {sku.imageUrl && (
+              {sku?.url && <span className="url">{sku.url.toString()}</span>}
+              {sku?.imageUrl && (
                 <span className="image_url">{sku.imageUrl.toString()}</span>
               )}
-              {sku.gtin && <span className="gtin">{sku.gtin}</span>}
-              {sku.availability && (
+              {sku?.gtin && <span className="gtin">{sku.gtin}</span>}
+              {sku?.availability && (
                 <span className="availability">{sku.availability}</span>
               )}
-              {sku.customFields &&
+              {sku?.customFields &&
                 Object.keys(sku.customFields).map(
                   (field, index) =>
                     sku.customFields &&

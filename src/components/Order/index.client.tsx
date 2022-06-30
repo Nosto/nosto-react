8af6829d428cd12e337a991022c61c6ebf8c1702
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-30 10:52:18 +0200

    added clientScriptLoaded and currentVariation to Order component

--- src/components/Order/index.client.tsx
+++ src/components/Order/index.client.tsx
@@ -1,27 +1,32 @@
 import { Purchase } from "../../types";
 import React, { useEffect } from "react";
 import snakeize from "snakeize";
+import { useNostoContext } from "../Provider/context.client";
 
 export interface OrderProps {
   purchase: Purchase;
 }
 
 const NostoOrder: React.FC<{ order: OrderProps }> = ({ order }) => {
+  const { clientScriptLoaded, currentVariation } = useNostoContext();
   useEffect(() => {
     // @ts-ignore
-    window.nostojs((api) => {
-      api
-        .defaultSession()
-        .setResponseMode("HTML")
-        .addOrder(snakeize(order))
-        .setPlacements(api.placements.getPlacements())
-        .load()
-        .then((data: object) => {
-          // @ts-ignore
-          api.placements.injectCampaigns(data.recommendations);
-        });
-    });
-  }, []);
+    if (clientScriptLoaded) {
+      window.nostojs((api: any) => {
+        api
+          .defaultSession()
+          .setVariation(currentVariation)
+          .setResponseMode("HTML")
+          .addOrder(snakeize(order))
+          .setPlacements(api.placements.getPlacements())
+          .load()
+          .then((data: object) => {
+            // @ts-ignore
+            api.placements.injectCampaigns(data.recommendations);
+          });
+      });
+    }
+  }, [clientScriptLoaded, currentVariation]);
 
   return (
     <>

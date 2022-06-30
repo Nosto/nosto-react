35348a3cb2275a4283e012d8198d28ea522a5940
Dominik Gilg <dominikgilg@Dominiks-MBP.fritz.box>
2022-06-30 10:37:43 +0200

    added clientScriptLoaded and currentVariation to to Checkout component

--- src/components/Checkout/index.client.tsx
+++ src/components/Checkout/index.client.tsx
@@ -1,21 +1,25 @@
 import React, { useEffect } from "react";
+import { useNostoContext } from "../Provider/context.client";
 
 const NostoCheckout: React.FC = () => {
+  const { clientScriptLoaded, currentVariation } = useNostoContext();
   useEffect(() => {
     // @ts-ignore
-    window.nostojs((api) => {
-      api
-        .defaultSession()
-        .setResponseMode("HTML")
-        .viewCart()
-        .setPlacements(api.placements.getPlacements())
-        .load()
-        .then((data: object) => {
-          // @ts-ignore
-          api.placements.injectCampaigns(data.recommendations);
-        });
-    });
-  }, []);
+    if (clientScriptLoaded) {
+      window.nostojs((api: any) => {
+        api
+          .defaultSession()
+          .setResponseMode("HTML")
+          .viewCart()
+          .setPlacements(api.placements.getPlacements())
+          .load()
+          .then((data: object) => {
+            // @ts-ignore
+            api.placements.injectCampaigns(data.recommendations);
+          });
+      });
+    }
+  }, [clientScriptLoaded, currentVariation]);
 
   return (
     <>

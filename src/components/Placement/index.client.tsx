9bd907484c3b3aa15bb2495999d965622c2bf52b
Dominik Gilg <74245822+dominikgilg@users.noreply.github.com>
2023-01-19 16:46:59 +0100

    Fix redirect issue in placement

--- src/components/Placement/index.client.tsx
+++ src/components/Placement/index.client.tsx
@@ -1,25 +1,11 @@
-import React, { MouseEvent } from "react";
+import React from "react";
 
 export interface PlacementProps {
   id: string;
 }
 
 const NostoPlacement: React.FC<PlacementProps> = ({ id }) => {
-  const handleClick = (e: MouseEvent) => {
-    // noinspection TypeScriptUnresolvedFunction
-    // @ts-ignore
-    const targetLink = e.target.closest("a");
-    if (!targetLink) {
-      return;
-    } else {
-      e.preventDefault();
-      location.href = targetLink.href
-        .toString()
-        .replace(new URL(targetLink.href).origin, "");
-    }
-  };
-
-  return <div className="nosto_element" onClick={handleClick} id={id} />;
+  return <div className="nosto_element" id={id} />;
 };
 
 export default NostoPlacement;

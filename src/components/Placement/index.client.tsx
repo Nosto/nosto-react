ca055fb3120070a553cbd1776bf864431cbb6a0f
Dominik Gilg <dominikgilg@Dominiks-MacBook-Pro.local>
2023-02-14 21:49:50 +0100

    added key for placement to solve ref issues when creating the root

--- src/components/Placement/index.client.tsx
+++ src/components/Placement/index.client.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 export interface PlacementProps {
   id: string;
+  pageType: string;
 }
 
-const NostoPlacement: React.FC<PlacementProps> = ({ id }) => {
-  return <div className="nosto_element" id={id} />;
+const NostoPlacement: React.FC<PlacementProps> = ({ id, pageType }) => {
+  return <div className="nosto_element" id={id} key={id + pageType} />;
 };
 
 export default NostoPlacement;

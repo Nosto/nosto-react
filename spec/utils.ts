46b811ad8558d5bd8a7ed14539e9d0f2b43bebdd
Timo Westkämper <timo.westkamper@nosto.com>
2024-07-22 16:42:34 +0300

    Add test case for product sku usage

--- spec/utils.ts
+++ spec/utils.ts
@@ -1,1 +1,7 @@
 export const WAIT_FOR_TIMEOUT = 2000
+
+export function listenTo(event: string) {
+  const requests: unknown[] = []
+  window.nostojs(api => api.listen(event, req => requests.push(req)))
+  return requests
+}

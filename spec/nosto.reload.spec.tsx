23791398c4841b7674d453e199b2666a0fcadecc
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-14 14:06:41 +0300

    Update tests

--- spec/nosto.reload.spec.tsx
+++ spec/nosto.reload.spec.tsx
@@ -15,25 +15,3 @@ test("verify Nosto is not loaded twice", async () => {
 
   expect(document.querySelector("[nosto-client-script]")).not.toBeInTheDocument()  
 })
-
-test("verify Shopify markets script is loaded", async () => {
-  render(
-    <NostoProvider account="shopify-11368366139" shopifyMarkets={{ language: "en", marketId: "123" }}>
-      <NostoHome />
-    </NostoProvider>
-  )
-
-  expect(document.querySelector("[nosto-client-script]")).toBeInTheDocument()
-  expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-language")).toBe("en")
-  expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-market-id")).toBe("123")
-})
-
-test("verify Nosto client script is loaded", async () => {
-  render(
-    <NostoProvider account="shopify-11368366139">
-      <NostoHome />
-    </NostoProvider>
-  )
-
-  expect(document.querySelector("[nosto-client-script]")).toBeInTheDocument()
-})

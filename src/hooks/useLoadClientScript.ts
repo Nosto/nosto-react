c7c7f50b427aa8ee3ff09bc4e37098434a5492b0
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-15 12:10:32 +0300

    Add loadScript flag

--- src/hooks/useLoadClientScript.ts
+++ src/hooks/useLoadClientScript.ts
@@ -3,10 +3,10 @@ import { isNostoLoaded } from "../components/helpers"
 import type { NostoClient } from "../types"
 import type { NostoProviderProps } from "../components/NostoProvider"
 
-type NostoScriptProps = Pick<NostoProviderProps, "account" | "host" | "shopifyMarkets">
+type NostoScriptProps = Pick<NostoProviderProps, "account" | "host" | "shopifyMarkets" | "loadScript">
 
 export function useLoadClientScript(props: NostoScriptProps) {
-  const { host, account, shopifyMarkets } = props
+  const { host, account, shopifyMarkets, loadScript = true } = props
   const [clientScriptLoadedState, setClientScriptLoadedState] = useState(false)
   const clientScriptLoaded = useMemo(() => clientScriptLoadedState, [clientScriptLoadedState])
 
@@ -38,44 +38,50 @@ export function useLoadClientScript(props: NostoScriptProps) {
       window.nostojs(api => api.setAutoLoad(false))
     }
 
-    // Load Nosto client script if not already loaded externally
-    if (!isNostoLoaded() && !shopifyMarkets) {
-      const urlPartial = `/include/${account}`
-      const script = createScriptElement(urlPartial)
-      script.onload = scriptOnload
-      document.body.appendChild(script)
-    }
-
-    // Load Shopify Markets scripts
-    if (shopifyMarkets) {
-      const existingScript = document.querySelector("[nosto-client-script]")
-      const marketId = String(shopifyMarkets?.marketId || "")
-      const language = shopifyMarkets?.language || ""
-
-      const existingScriptAttributes =
-      existingScript?.getAttribute("nosto-language") !== language ||
-      existingScript?.getAttribute("nosto-market-id") !== marketId
-
-      if (!existingScript || existingScriptAttributes) {
-        if (clientScriptLoadedState) {
-          setClientScriptLoadedState(false)
-        }
-
-        const nostoSandbox = document.querySelector("#nosto-sandbox")
-
-        existingScript?.parentNode?.removeChild(existingScript)
-        nostoSandbox?.parentNode?.removeChild(nostoSandbox)
-
-        const urlPartial =
-        `/script/shopify/market/nosto.js?merchant=${account}&market=${marketId}&locale=${language.toLowerCase()}`
+    if (loadScript) {
+      // Load Nosto client script if not already loaded externally
+      if (!isNostoLoaded() && !shopifyMarkets) {
+        const urlPartial = `/include/${account}`
         const script = createScriptElement(urlPartial)
-        script.setAttribute("nosto-language", language)
-        script.setAttribute("nosto-market-id", marketId)
         script.onload = scriptOnload
         document.body.appendChild(script)
       }
-    }
 
+      // Load Shopify Markets scripts
+      if (shopifyMarkets) {
+        const existingScript = document.querySelector("[nosto-client-script]")
+        const marketId = String(shopifyMarkets?.marketId || "")
+        const language = shopifyMarkets?.language || ""
+
+        const existingScriptAttributes =
+          existingScript?.getAttribute("nosto-language") !== language ||
+          existingScript?.getAttribute("nosto-market-id") !== marketId
+
+        if (!existingScript || existingScriptAttributes) {
+          if (clientScriptLoadedState) {
+            setClientScriptLoadedState(false)
+          }
+
+          const nostoSandbox = document.querySelector("#nosto-sandbox")
+
+          existingScript?.parentNode?.removeChild(existingScript)
+          nostoSandbox?.parentNode?.removeChild(nostoSandbox)
+
+          const urlPartial =
+            `/script/shopify/market/nosto.js?merchant=${account}&market=${marketId}&locale=${language.toLowerCase()}`
+          const script = createScriptElement(urlPartial)
+          script.setAttribute("nosto-language", language)
+          script.setAttribute("nosto-market-id", marketId)
+          script.onload = scriptOnload
+          document.body.appendChild(script)
+        }
+      }
+    } else {
+      window.nosto?.reload({
+        site: "localhost",
+      })
+      setClientScriptLoadedState(true)
+    }
   }, [clientScriptLoadedState, shopifyMarkets])
 
   return { clientScriptLoaded }

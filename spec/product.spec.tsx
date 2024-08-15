5f1a43410700a89a862b91c2bc8b4731532c855a
Timo Westkämper <timo.westkamper@nosto.com>
2024-08-15 15:22:20 +0300

    Cleanup of tests

--- spec/product.spec.tsx
+++ spec/product.spec.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import { test, expect } from "vitest"
 import { render, screen, waitFor } from "@testing-library/react"
 import { NostoProvider, NostoPlacement, NostoProduct } from "../src/index"
--- spec/renderer.tsx
+++ spec/renderer.tsx
@@ -1,4 +1,3 @@
-import React from "react"
 import type { Recommendation } from "../src/index"
 
 interface RecommendationProps {

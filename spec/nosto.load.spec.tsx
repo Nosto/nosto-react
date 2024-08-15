a0433a509c665ea1c952e2afb88ad64f1c541bb9
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-15 10:01:39 +0300

    Add Nosto client script load spec

---
+++ spec/nosto.load.spec.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { vi, describe, expect, it } from "vitest"
+import { render } from "@testing-library/react"
+import { NostoProvider, NostoHome } from "../src/index"
+
+describe("Nosto client script loading", () => {
+  it("is stopped via provider when the script is loaded externally", async () => {
+    window.nosto = {
+      reload: vi.fn()
+    }
+
+    render(
+      <NostoProvider account="shopify-11368366139" loadScript={false}>
+        <NostoHome />
+      </NostoProvider>
+    )
+
+    expect(document.querySelector("[nosto-client-script]")).not.toBeInTheDocument()
+  })
+
+  it("is loaded", () => {  
+    render(
+      <NostoProvider account="shopify-11368366139">
+        <NostoHome />
+      </NostoProvider>
+    )
+
+    expect(document.querySelector("[nosto-client-script]")).toBeInTheDocument()
+  })
+
+  it("Shopify markets script", () => {
+    render(
+      <NostoProvider account="shopify-11368366139" shopifyMarkets={{ language: "en", marketId: "123" }}>
+        <NostoHome />
+      </NostoProvider>
+    )
+
+    expect(document.querySelector("[nosto-client-script]")).toBeInTheDocument()
+    expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-language")).toBe("en")
+    expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-market-id")).toBe("123")
+  })
+})

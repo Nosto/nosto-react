7ef53eab2fe37f9013e57c0cbe63eb7c6536f858
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-15 13:59:30 +0300

    Add more test cases, review feedback

--- spec/nosto.load.spec.tsx
+++ spec/nosto.load.spec.tsx
@@ -1,10 +1,11 @@
-import { describe, expect, it } from "vitest"
-import { render } from "@testing-library/react"
+import { describe, expect, it, vi } from "vitest"
+import { render, renderHook } from "@testing-library/react"
 import "@testing-library/jest-dom/vitest"
 import { NostoProvider, NostoHome } from "../src/index"
+import { useLoadClientScript } from "../src/hooks"
 
-describe("Nosto client script loading", () => {
-  it("verify Nosto is not loaded twice", () => {
+describe("Nosto client script", () => {
+  it("verify is not loaded twice", () => {
     // @ts-expect-error dummy placeholder for Nosto iframe window scope  
     window.nosto = {}
 
@@ -37,5 +38,25 @@ describe("Nosto client script loading", () => {
     expect(document.querySelector("[nosto-client-script]")).toBeInTheDocument()
     expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-language")).toBe("en")
     expect(document.querySelector("[nosto-client-script]")?.getAttribute("nosto-market-id")).toBe("123")
+  })
+
+  it("should set loaded state to true when client is loaded", () => {
+    const hook = renderHook(() => useLoadClientScript({ loadScript: false, account: "shopify-11368366139" }))
+    expect(hook.result.current.clientScriptLoaded).toBe(true)
+  })
+
+  it("remove existing scripts before loading new ones", () => {
+    const existingScript = document.createElement("script")
+    existingScript.setAttribute("nosto-client-script", "")
+    document.body.appendChild(existingScript)
+
+    const nostoSandbox = document.createElement("div")
+    nostoSandbox.id = "nosto-sandbox"
+    document.body.appendChild(nostoSandbox)
+
+    renderHook(() => useLoadClientScript({ loadScript: true, account: "shopify-11368366139", shopifyMarkets: { marketId: "123", language: "en" } }))
+
+    expect(document.body.contains(existingScript)).toBe(false)
+    expect(document.body.contains(nostoSandbox)).toBe(false)
   })
 })
--- src/hooks/useLoadClientScript.ts
+++ src/hooks/useLoadClientScript.ts
@@ -39,44 +39,45 @@ export function useLoadClientScript(props: NostoScriptProps) {
     }
 
     if (!loadScript) {
-      setClientScriptLoadedState(true)
-    } else {
-      // Load Nosto client script if not already loaded externally
-      if (!isNostoLoaded() && !shopifyMarkets) {
-        const urlPartial = `/include/${account}`
+      scriptOnload()
+      return
+    }
+
+    // Load Nosto client script if not already loaded externally
+    if (!isNostoLoaded() && !shopifyMarkets) {
+      const urlPartial = `/include/${account}`
+      const script = createScriptElement(urlPartial)
+      script.onload = scriptOnload
+      document.body.appendChild(script)
+    }
+
+    // Load Shopify Markets scripts
+    if (shopifyMarkets) {
+      const existingScript = document.querySelector("[nosto-client-script]")
+      const marketId = String(shopifyMarkets?.marketId || "")
+      const language = shopifyMarkets?.language || ""
+
+      const existingScriptAttributes =
+        existingScript?.getAttribute("nosto-language") !== language ||
+        existingScript?.getAttribute("nosto-market-id") !== marketId
+
+      if (!existingScript || existingScriptAttributes) {
+        if (clientScriptLoadedState) {
+          setClientScriptLoadedState(false)
+        }
+
+        const nostoSandbox = document.querySelector("#nosto-sandbox")
+
+        existingScript?.parentNode?.removeChild(existingScript)
+        nostoSandbox?.parentNode?.removeChild(nostoSandbox)
+
+        const urlPartial =
+          `/script/shopify/market/nosto.js?merchant=${account}&market=${marketId}&locale=${language.toLowerCase()}`
         const script = createScriptElement(urlPartial)
+        script.setAttribute("nosto-language", language)
+        script.setAttribute("nosto-market-id", marketId)
         script.onload = scriptOnload
         document.body.appendChild(script)
-      }
-
-      // Load Shopify Markets scripts
-      if (shopifyMarkets) {
-        const existingScript = document.querySelector("[nosto-client-script]")
-        const marketId = String(shopifyMarkets?.marketId || "")
-        const language = shopifyMarkets?.language || ""
-
-        const existingScriptAttributes =
-          existingScript?.getAttribute("nosto-language") !== language ||
-          existingScript?.getAttribute("nosto-market-id") !== marketId
-
-        if (!existingScript || existingScriptAttributes) {
-          if (clientScriptLoadedState) {
-            setClientScriptLoadedState(false)
-          }
-
-          const nostoSandbox = document.querySelector("#nosto-sandbox")
-
-          existingScript?.parentNode?.removeChild(existingScript)
-          nostoSandbox?.parentNode?.removeChild(nostoSandbox)
-
-          const urlPartial =
-            `/script/shopify/market/nosto.js?merchant=${account}&market=${marketId}&locale=${language.toLowerCase()}`
-          const script = createScriptElement(urlPartial)
-          script.setAttribute("nosto-language", language)
-          script.setAttribute("nosto-market-id", marketId)
-          script.onload = scriptOnload
-          document.body.appendChild(script)
-        }
       }
     }
   }, [clientScriptLoadedState, shopifyMarkets])

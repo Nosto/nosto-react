dd3451fa090de6aac1f9ea334bf450e06db36b41
Mikko Poutanen <12759991+nosto-mikpou@users.noreply.github.com>
2024-08-15 15:08:50 +0300

    Rename test, destruct result from renderHook

--- spec/useLoadClientScript.spec.tsx
+++ spec/useLoadClientScript.spec.tsx
@@ -3,12 +3,12 @@ import { renderHook } from "@testing-library/react"
 import { useLoadClientScript } from "../src/hooks"
 
 describe("useLoadClientScript", () => {
-  it("set loaded state to true when client is loaded", () => {
-    const hook = renderHook(() => useLoadClientScript({ loadScript: false, account: "shopify-11368366139" }))
-    expect(hook.result.current.clientScriptLoaded).toBe(true)
+  it("set loaded state to true when client is loaded externally", () => {
+    const { result } = renderHook(() => useLoadClientScript({ loadScript: false, account: "shopify-11368366139" }))
+    expect(result.current.clientScriptLoaded).toBe(true)
   })
 
-  it("remove existing scripts before loading new ones", () => {
+  it("remove existing Shopify markets related scripts before loading new ones", () => {
     const existingScript = document.createElement("script")
     existingScript.setAttribute("nosto-client-script", "")
     document.body.appendChild(existingScript)
@@ -17,7 +17,7 @@ describe("useLoadClientScript", () => {
     nostoSandbox.id = "nosto-sandbox"
     document.body.appendChild(nostoSandbox)
 
-    renderHook(() => useLoadClientScript({ loadScript: true, account: "shopify-11368366139", shopifyMarkets: { marketId: "123", language: "en" } }))
+    const { result } = renderHook(() => useLoadClientScript({ account: "shopify-11368366139", shopifyMarkets: { marketId: "123", language: "en" } }))
 
     expect(document.body.contains(existingScript)).toBe(false)
     expect(document.body.contains(nostoSandbox)).toBe(false)
